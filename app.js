991a9f00f8dc2fbdf5b2b809a028e8a6951eea67
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-07-03 14:23:00 +0300

    Enums

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-//object
+//ENUM
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 3] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 10] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Andreea',
     age: 21,
     hobbies: ['f1', 'cubing'],
+    role: Role.ADMIN,
 };
 //with any type we can add any type of data in the array
 // but is not ok to use it because we lose the type checking
@@ -12,4 +19,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.ADMIN) {
+    console.log('is author');
 }
--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-//object
-
+//ENUM
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 3,
+  AUTHOR = 10,
+}
 const person = {
   name: 'Andreea',
   age: 21,
   hobbies: ['f1', 'cubing'],
+  role: Role.ADMIN,
 }
 //with any type we can add any type of data in the array
 // but is not ok to use it because we lose the type checking
@@ -15,4 +20,8 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
   console.log(hobby)
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is admin')
 }
