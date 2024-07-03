8fb567137860da2e4f463b5d5bf36c5a313e6bbf
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-07-03 10:44:25 +0300

    Convert add function from JavaScript to TypeScript

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('helo, helo');
+// function add(n1, n2) {
+//   return n1 + n2
+// }
+// const number1 = 3
+// const number2 = 2.6
+// const result = add(number1, number2)
+// console.log(result)
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = '5';
+var number2 = 2.6;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log('helo, heloo')
+// function add(n1, n2) {
+//   return n1 + n2
+// }
+
+// const number1 = 3
+// const number2 = 2.6
+
+// const result = add(number1, number2)
+// console.log(result)
+
+function add(n1: number, n2: number) {
+  return n1 + n2
+}
+
+const number1 = 5
+const number2 = 2.6
+
+const result = add(number1, number2)
+console.log(result)
