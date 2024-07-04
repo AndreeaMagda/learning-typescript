3de358144c42f7ae7e1f50d6d77d36951253b1db
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-07-04 11:33:31 +0300

    Function Types & Callbacks

--- functions.js
+++ functions.js
@@ -4,7 +4,16 @@ function add_fc(n1, n2) {
 function printResult(num) {
     console.log('Result  ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add_fc(2, 32));
+//whatever we store here, has to be a function that takes two numbers and returns a number
 var combineValues;
 combineValues = add_fc;
+//combineValues=printResult -ERROR
 console.log(combineValues(45, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- functions.ts
+++ functions.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log('Result  ' + num)
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2
+  cb(result)
+}
+
 printResult(add_fc(2, 32))
 
 //whatever we store here, has to be a function that takes two numbers and returns a number
@@ -15,3 +20,7 @@ combineValues = add_fc
 //combineValues=printResult -ERROR
 
 console.log(combineValues(45, 8))
+
+addAndHandle(10, 20, (result) => {
+  console.log(result)
+})
