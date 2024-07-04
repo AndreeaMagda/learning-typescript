3910e7fa1f139f10bf6b114eeec8f1d4040171ff
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-07-04 18:41:34 +0300

    Multiple decorators

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
-function Logger(constructor: Function) {
-  console.log('Logging...')
-  console.log(constructor)
+function Logger(logString: string) {
+  console.log('LOGGER FACTORY')
+
+  return function (constructor: Function) {
+    console.log(logString)
+    console.log(constructor)
+  }
 }
 
-@Logger
+function WithTemplate(template: string, hookId: string) {
+  console.log('LOGGER TEMPLATE')
+  return function (constructor: any) {
+    console.log('Rendering template')
+    const hookEl = document.getElementById(hookId)
+    const p = new constructor()
+    if (hookEl) {
+      hookEl.innerHTML = template
+      hookEl.querySelector('h1')!.textContent = p.name
+    }
+  }
+}
+
+@Logger('Logging')
+@WithTemplate('<h1>My person Object</h1>', 'app')
 class Person {
   name = 'Charles'
 
