fb2b17d9a5cc51087924cc1f6c037560f2a1caf1
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-07-04 13:37:25 +0300

    Rest Parameters

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,18 @@
 "use strict";
 //arrow functions
-const addNumb = (a, b) => a + b;
-const printOutput = (output) => console.log(output);
-const button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', (event) => console.log(event));
-}
-printOutput(addNumb(5, 2));
+// const addNumb = (a: number, b: number) => a + b
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output)
+// const button = document.querySelector('button')
+// if (button) {
+//   button.addEventListener('click', (event) => console.log(event))
+// }
+// printOutput(addNumb(5, 2))
+//rest parameters
+const addNumbers = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = addNumbers(5, 2, 8, 7, 2, 8);
+console.log(addedNumbers);
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
 //arrow functions
 
-const addNumb = (a: number, b: number) => a + b
+// const addNumb = (a: number, b: number) => a + b
 
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output)
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output)
 
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 
-if (button) {
-  button.addEventListener('click', (event) => console.log(event))
+// if (button) {
+//   button.addEventListener('click', (event) => console.log(event))
+// }
+
+// printOutput(addNumb(5, 2))
+
+//rest parameters
+
+const addNumbers = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue
+  }, 0)
 }
 
-printOutput(addNumb(5, 2))
+const addedNumbers = addNumbers(5, 2, 8, 7, 2, 8)
+console.log(addedNumbers)
