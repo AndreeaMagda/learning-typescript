8be5134e023bb49ce338091323565b433d5ae3a8
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-07-04 13:11:42 +0300

    Arrow function

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,9 @@
 "use strict";
-const userName = 'landoooo';
-console.log(userName);
+//arrow functions
+const addNumb = (a, b) => a + b;
+const printOutput = (output) => console.log(output);
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', (event) => console.log(event));
+}
+printOutput(addNumb(5, 2));
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-const userName = 'landoooo'
+//arrow functions
 
-console.log(userName)
+const addNumb = (a: number, b: number) => a + b
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output)
+
+const button = document.querySelector('button')
+
+if (button) {
+  button.addEventListener('click', (event) => console.log(event))
+}
+
+printOutput(addNumb(5, 2))
